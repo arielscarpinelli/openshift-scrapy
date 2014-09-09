0bff1346eca355e378d9f6a9095642e61bb74e61
Ariel Scarpinelli <triforcexp@gmail.com>
2014-09-09 17:15:38 -0300

    First attempt

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='openshift-scrapy',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='Scrapy on openshift',
+      author='Ariel Scarpinelli',
+      author_email='triforcexp AT gmail',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Scrapy', 'Scrapyd'],
      )
