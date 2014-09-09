1f53313a39ebec1fe26a19db75c74e5c3dd8d285
Ariel Scarpinelli <triforcexp@gmail.com>
2014-09-09 17:53:32 -0300

    Second try

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from twisted.scripts.twistd import run
-from os.path import join, dirname
-from sys import argv
-import scrapyd
-argv[1:1] = ['-n', '-y', join(dirname(scrapyd.__file__), 'txapp.py')]
-run()
+from scrapyd import get_application
+from twisted.internet import reactor
+from twisted.application import app
+
+application = get_application(config)
+app.startApplication(application, False)
+reactor.run()
